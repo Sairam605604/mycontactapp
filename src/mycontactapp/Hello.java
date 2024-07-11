df77eace3f16de451eb606651f36c70e0d0e59fa
Sairam605604 <sairamdoolla123@gmail.com>
2024-07-11 16:39:45 +0530

    Update Hello.java

--- src/mycontactapp/Hello.java
+++ src/mycontactapp/Hello.java
@@ -3,7 +3,7 @@ package mycontactapp;
 public class Hello {
   public static void main(String[] args) {
 		System.out.println("Hello...everybody");
-	  	System.out.println("Hi")
+	  	System.out.println("Hi");
 	}
  
 
